e5273a4d48d866bf7e5286056531150dd8fdf3df
amsaif52 <amsaif52@gmail.com>
2021-04-30 18:49:07 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var request = require('request');
+var express = require('express');
+var parser = require('fast-xml-parser');
+var app = express();
+var PORT = process.env.PORT || 3001;
+
+app.get("/", function(req, res) {
+    const requestString = req.query.requestString;
+    console.log(requestString);
+    var options = {
+      'method': 'GET',
+      'url': `https://returns.usps.com/services/GetLabel?externalReturnLabelRequest=${requestString}`
+    };
+    request(options, function (error, response) {
+        if (error) throw new Error(error);
+        var xmlData = response.body;
+        if( parser.validate(xmlData) === true) { //optional (it'll return an object in case it's not valid)
+            var jsonObj = parser.parse(xmlData,options);
+        }
+        var base64string = jsonObj.ExternalReturnLabelResponse.ReturnLabel;
+        var data = [];
+        data.push(Buffer.from(base64string, 'base64'));
+        data = Buffer.concat(data);
+        console.log(data);
+        console.log('requested content length: ', response.headers['content-length']);
+        console.log('parsed content length: ', data.length);
+        res.writeHead(200, {
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': 'attachment; filename=usps-tracking.pdf',
+            'Content-Length': data.length
+        });
+        res.end(data);
+    });
+});
+
+app.listen(PORT, function(err){
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
